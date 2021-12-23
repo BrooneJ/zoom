96fc883f62714026b76a0f31e2c86532bf8e23bb
HyungwonJin <86827200+HyungwonJin@users.noreply.github.com>
2021-12-24 00:17:36 +0900

    chore: front에서 json을 만들어서 back으로 전송

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,8 +1,14 @@
 // backend와 connection을 열어줌
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
+const messageForm = document.querySelector("#message");
+const nickForm = document.querySelector("#nickname");
 
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 // 서버와 연결이 되면 실행
 socket.addEventListener("open", () => {
@@ -11,7 +17,9 @@ socket.addEventListener("open", () => {
 
 // 서버로부터 받은 데이터를 표시
 socket.addEventListener("message", (message) => {
-  console.log("New message: ", message.data);
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 // 서버와 연결이 끊어졌을 경우 실행
@@ -22,8 +30,15 @@ socket.addEventListener("close", () => {
 function handleSubmit(event) {
   event.preventDefault();
   const input = messageForm.querySelector("input");
-  socket.send(input.value);
+  socket.send(makeMessage("new_message", input.value));
   input.value = "";
 }
 
+function handleNickSubmit(event) {
+  event.preventDefault();
+  const input = nickForm.querySelector("input");
+  socket.send(makeMessage("nickname", input.value));
+}
+
 messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
